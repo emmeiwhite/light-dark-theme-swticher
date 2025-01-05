8d1f087b77fa00ee01e4c4f136afc4e95668298c
emmeiwhite <emmeiwhite@gmail.com>
2025-01-05 23:22:52 +0530

    light-dark-1.3 changing color of all text as theme is switched!

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -4,13 +4,11 @@ const Paragraph = ({ children }) => {
   const { theme } = useTheme();
   console.log(theme);
   return (
-    <p
-      style={{
-        color: theme === "light" ? "black" : "white",
-      }}
-    >
-      {children}
-    </p>
+    <div style={{ color: theme === "light" ? "black" : "white" }}>
+      <p>{children}</p>
+
+      <p style={{ fontSize: "2rem" }}>Current Theme is {theme}</p>
+    </div>
   );
 };
 
