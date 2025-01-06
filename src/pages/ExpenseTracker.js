d5223e32809e08caf69fa61f8f05ec2c5b29e90d
emmeiwhite <emmeiwhite@gmail.com>
2025-01-06 16:39:24 +0530

    light-dark-1.5 Expense Tracker light-dark-theme!

--- src/pages/ExpenseTracker.js
+++ src/pages/ExpenseTracker.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import { useTheme } from "../ThemeContext";
 
 const initialState = {
   moneyForWeek: 1000,
@@ -12,10 +13,15 @@ const reducer = (state, action) => {
   if (action.type === "sell_meal") {
     return { ...state, moneyForWeek: state.moneyForWeek + action.payload };
   }
+
+  if (action.type === "celebrity_visit") {
+    return { ...state, moneyForWeek: state.moneyForWeek + action.payload };
+  }
 };
 
 export default function ExpenseTracker() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { theme } = useTheme();
 
   const handleBuying = () => {
     dispatch({ type: "buy_stuff", payload: 10 });
@@ -24,11 +30,16 @@ export default function ExpenseTracker() {
   const handleSelling = () => {
     dispatch({ type: "sell_meal", payload: 15 });
   };
+
+  const handleCelebrity = () => {
+    dispatch({ type: "sell_meal", payload: 5000 });
+  };
   return (
-    <div>
+    <div style={{ color: theme === "light" ? "black" : "white" }}>
       <h1>Currently Weekly Money :{state.moneyForWeek}</h1>
       <button onClick={handleBuying}>Buy Veggies for $10</button>
       <button onClick={handleSelling}>Sell meals for $15</button>
+      <button onClick={handleCelebrity}>hosting a celebrity</button>
     </div>
   );
 }
