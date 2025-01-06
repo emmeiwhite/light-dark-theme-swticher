780482a25cd8504299088cc09ebc8a6f5e3c56b4
emmeiwhite <emmeiwhite@gmail.com>
2025-01-06 21:02:28 +0530

    light-dark-1.9 React.Children.map()

--- src/LiveOrders/index.js
+++ src/LiveOrders/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./Style.css";
 
 // The row class sets everything in flex one line, we need to add space!
@@ -7,9 +8,20 @@ const Row = ({ children }) => {
   return <div className="row">{children}</div>;
 };
 --- */
+
+const Row = ({ children }) => {
+  return (
+    <div className="row">
+      {React.Children.map(children, (child, index) => {
+        return child;
+      })}
+    </div>
+  );
+};
 export default function LiveOrders() {
   return (
     <div>
+      <h3>Delving Deep into JSX World!</h3>
       <Row>
         <p>Margeretta</p>
         <p>Quanity 2</p>
